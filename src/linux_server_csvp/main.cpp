2aa8ccc6d1f859b646b2180240a50cb04075acc1
Peter Artemyev <petrkcheli@gmail.com>
2024-07-20 05:21:19 +0500

    test commit linux server

--- src/linux_server_csvp/main.cpp
+++ src/linux_server_csvp/main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 
 // главная функция работы с двумя клиентами
 void work_client(Server* server, int socket_client1, int socket_client2,
-                 Server::Player data_player1, Server::Player data_player2){
+                 Server::Player data_player1, Server::Player data_player2,
+                 Server::BulletManager bullets_player1, Server::BulletManager bullets_player2){
 
     //эта строчка отработает только один раз, когда начнется игра
     //отправляем клиенту его начальные координаты
@@ -55,7 +56,21 @@ void work_client(Server* server, int socket_client1, int socket_client2,
 
             system("pause");
         }
-        std::cerr << "player1 -> data1" << std::endl;
+        std::cerr << "player2 -> data2" << std::endl;
+
+        if(server->recv_client(socket_client1, bullets_player1) < 0) {
+            delete server;
+
+            system("pause");
+        }
+        std::cerr << "player1 -> bullet1" << std::endl;
+
+        if(server->recv_client(socket_client2, data_player2) < 0) {
+            delete server;
+
+            system("pause");
+        }
+        std::cerr << "player2 -> bullet2" << std::endl;
 
         if(server->send_client(socket_client2, data_player1) < 0){
             delete server;
@@ -68,6 +83,18 @@ void work_client(Server* server, int socket_client1, int socket_client2,
             system("pause");
         }
         std::cerr << "player2 -> data1" << std::endl;
+
+        if(server->send_client(socket_client2, bullets_player1) < 0){
+            delete server;
+            system("pause");
+        }
+        std::cerr << "player2 -> bullets1" << std::endl;
+
+        if(server->send_client(socket_client1, bullets_player2) < 0){
+            delete server;
+            system("pause");
+        }
+        std::cerr << "player2 -> bullets2" << std::endl;
     }
 }
 
